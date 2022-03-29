034aef20e833ee9007cf0230d4f70668dd6cdbb3
ILuckyBROI <qalq1@bk.ru>
2022-03-29 15:33:58 +0300

    Added activation key

--- users/models.py
+++ users/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+from datetime import timedelta
 
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='base_image', null=True, blank=True)
+
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=8)), null=True, blank=True)
 
     def safe_active(self):
         self.is_active = True
@@ -12,3 +17,12 @@ class User(AbstractUser):
     def safe_delete(self):
         self.is_active = False
         self.save()
+
+    @property
+    def is_activation_key_expired(self):
+        try:
+            if now() <= self.activation_key_expires:
+                return False
+        except Exception as e:
+            pass
+        return True
