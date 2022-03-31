69103fcecc22d8db03baf263a0f0fabc507ecdbb
ILuckyBROI <qalq1@bk.ru>
2022-03-31 14:47:51 +0300

    Added url login backend

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
     path('adminko/', include('adminko.urls', namespace='adminko')),
+    path('', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
