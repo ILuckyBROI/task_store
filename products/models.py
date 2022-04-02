2f22026e4ed9b5e2a5b4728b160bbd8a0f3102ab
ILuckyBROI <qalq1@bk.ru>
2022-04-02 09:50:52 +0300

    Update connection

--- products/models.py
+++ products/models.py
@@ -11,9 +11,16 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
+    AVAILABLE_IN_STOCK = 'Есть'
+    OUT_OF_STOCK = 'Нет'
+    CHOOSING_AVAILABILITY = (
+        (AVAILABLE_IN_STOCK, 'Есть в наличии'),
+        (OUT_OF_STOCK, 'Нет в наличии')
+    )
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='product_images', blank=True, null=True)
-    availability = models.CharField(max_length=20, default='Есть в наличии')
+    availability = models.CharField(verbose_name='Наличие товара', max_length=20, choices=CHOOSING_AVAILABILITY,
+                                    default='Есть в наличии')
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, default='Новинки')
